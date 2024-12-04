13f0ba8fa3d26bfedad84f20a546fa1e42e43c65
Krish-Kalyani <kkalyani1@student.gsu.edu>
2024-12-04 14:23:48 -0500

    fixed all files

--- js:validation.js
+++
@@ -1,8 +0,0 @@
-function validateForm() {
-    const username = document.getElementById('username').value;
-    const password = document.getElementById('password').value;
-    if (username.length < 3 || password.length < 6) {
-        alert("Username must be at least three characters and password at least six characters.");
-        return false; }
-        return true;
-    }
